4d2420f751b41a7b780b6c35f041e00501f0cb86
harystyleseze <harystyleseze@gmail.com>
2024-11-21 06:54:01 +0000

    user task

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,31 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""_SQLAlchemy for user model to store user data with attributes._
 """
-SQLAlchemy for user model to store user data with attributes.
-"""
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
 
-# Base class for Declarative base 
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 class User(Base):
-    """
-    User model that inherits the declarative base and stores user data.
+    """_User model that inherits the declarative base and stores user data._
 
-    Attributes:
-        id (Int): A primary key unique to each user.
-        email (Str): A non-nullable string for the user's email address.
-        hashed_password (Str): A non-nullable string for user authentication.
-        session_id (Str): A nullable string for tracking user sessions.
-        reset_token (Str): A nullable string for recovering the user's password.
+    Args:
+        Base (_type_): _description_
     """
-    # Database table name
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    # Column definitions
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
